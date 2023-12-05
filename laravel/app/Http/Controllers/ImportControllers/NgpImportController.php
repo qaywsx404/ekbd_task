10aabc28839d0340be9794dccddc8bf16fbde0b0
qaywsx404 <96451497+qaywsx404@users.noreply.github.com>
2023-12-05 18:06:12 +0300

    Ngp change hash

--- laravel/app/Http/Controllers/ImportControllers/NgpImportController.php
+++ laravel/app/Http/Controllers/ImportControllers/NgpImportController.php
@@ -9,26 +9,36 @@ use App\Models\ebd_gis\Ngp2019;
 
 class NgpImportController extends Controller
 {
-    public static function import()
+    /** Доп. инф. */
+    private static $showInf = false;
+
+    /**
+     * Начать импорт
+     * 
+     * @param bool $showInf  `true`  - показывать доп. инф.
+     *                       `false` - не показывать доп. инф.
+     */
+    public static function import(bool $showInf = false)
     {
-        $newCount = $unsavedCount = 0;
+        self::$showInf = $showInf;
+        $newCount = 0;
 
-        [$newCount, $unsavedCount] = self::importFromTable();
+        Ngp::truncate();
+        if(self::$showInf) dump("   Ngp tr");
 
-        dump("Ngp total: Added " . $newCount . ', unsaved ' . $unsavedCount);
+        $newCount = self::importFromTable();
+
+        dump("Ngp total: Added " . $newCount . ' of ' . Ngp2019::count());
     }
 
     /**  Импорт записей из таблицы  ebd_gis.ngp_2019 */
-    private static function importFromTable() : array
+    private static function importFromTable() : int
     {
-        $newCount = $unsavedCount = 0;
+        $newCount = 0;
 
             foreach(Ngp2019::all() as $n)
             {
-
                 $newNgp = Ngp::create([
-                    //TODO
-                    //'vid'
                     'name' => $n->province,
                     'ngp_type_id' => $n->type_ngp ? DicNgpType::where('value', $n->type_ngp)->first()->id : null,
                     'index_all' => $n->index_all,
@@ -36,19 +46,9 @@ class NgpImportController extends Controller
                     'geom' => $n->geom
                 ]);
         
-                $newNgp->refresh();
-
-                if( count(Ngp::where('src_hash', $newNgp->src_hash)->get()) > 1)
-                {
-                    $newNgp->delete();
-                    $unsavedCount++;
-                }
-                else
-                {
-                    $newCount++;
-                }
+                $newCount++;
             }
 
-        return [$newCount, $unsavedCount];
+        return $newCount;
     }
 }
